07b9aae0640470da3a737a2795122a9bfd95e9c9
Steve Persch <steve.persch@pantheon.io>
2020-01-30 17:23:06 -0600

    more hacky proxy fun

--- composer-path-repos/libraries/hacky-proxy/src/PantheonToGCPBucket.php
+++ composer-path-repos/libraries/hacky-proxy/src/PantheonToGCPBucket.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Stevector\HackyProxy;
+
 use Proxy\Proxy;
 use Proxy\Adapter\Guzzle\GuzzleAdapter;
 use Proxy\Filter\RemoveEncodingFilter;
@@ -7,46 +9,59 @@ use Zend\Diactoros\ServerRequestFactory;
 
 
 
-if  (1===3) {
+class PantheonToGCPBucket {
 
 
-  $fake_server = [
+  function __construct()
+  {
+
+
+    if  (1===3) {
+
+
+      $fake_server = [
 //  'REQUEST_URI' =>  '/pr-19' . $_SERVER['REQUEST_URI']
-  ];
+      ];
 
-  $fake_server = $_SERVER;
-  $fake_server['REQUEST_URI'];
+      $fake_server = $_SERVER;
+      $fake_server['REQUEST_URI'];
 
-  if (empty($fake_server['REQUEST_URI']) || $fake_server['REQUEST_URI'] == '/') {
-    $fake_server['REQUEST_URI'] = '/index.html';
-  }
+      if (empty($fake_server['REQUEST_URI']) || $fake_server['REQUEST_URI'] == '/') {
+        $fake_server['REQUEST_URI'] = '/index.html';
+      }
 
-  $fake_server['REQUEST_URI'] = '/' . $_ENV['PANTHEON_ENVIRONMENT'] . $fake_server['REQUEST_URI'];
+      $fake_server['REQUEST_URI'] = '/' . $_ENV['PANTHEON_ENVIRONMENT'] . $fake_server['REQUEST_URI'];
 
-  $request = ServerRequestFactory::fromGlobals($fake_server);
+      $request = ServerRequestFactory::fromGlobals($fake_server);
 
 // Create a guzzle client
-  $guzzle = new GuzzleHttp\Client();
+      $guzzle = new GuzzleHttp\Client();
 
 // Create the proxy instance
-  $proxy = new Proxy(new GuzzleAdapter($guzzle));
+      $proxy = new Proxy(new GuzzleAdapter($guzzle));
 
 // Add a response filter that removes the encoding headers.
-  $proxy->filter(new RemoveEncodingFilter());
+      $proxy->filter(new RemoveEncodingFilter());
 
-  $url = 'http://gcp-gatsby-bucket.stevector.com/';
+      $url = 'http://gcp-gatsby-bucket.stevector.com/';
 
 //$url = rtrim($url, '/');
 // Forward the request and get the response.
-  $response = $proxy->forward($request)->to($url);
+      $response = $proxy->forward($request)->to($url);
 
 
 
 //print_r($response);
 
 // Output response to the browser.
-  (new Zend\Diactoros\Response\SapiEmitter)->emit($response);
-  print_r($url);
-  die();
+      (new Zend\Diactoros\Response\SapiEmitter)->emit($response);
+      print_r($url);
+      die();
+    }
+
+
+
+  }
+
 }
 
